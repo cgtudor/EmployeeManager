6d5ad631ad82daa51581d0ee0ca2268c68457c7c
Cristian <tudor.cristian.gabriel@gmail.com>
2019-09-23 17:36:10 +0100

    Added extra hours

---
+++ EmployeeManager/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmployeeManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the hours worked");
+            int hours = Int32.Parse(Console.ReadLine());
+            double wage = hours <= 40 ? hours * 9.5 : 40 * 9.5 + (hours-40) * 14.25;
+            Console.WriteLine("The weekly wage is " + string.Format("{0:0.00}", wage));
+            //Employee done
+            Console.WriteLine("wow");
+        }
+    }
+}
