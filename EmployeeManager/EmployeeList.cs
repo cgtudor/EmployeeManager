90722a6d382f4a49d21c6d2137d429f329d9fd6b
Cristian <tudor.cristian.gabriel@gmail.com>
2019-09-23 20:12:27 +0100

    Added menu for adding, removing and listing employees

---
+++ EmployeeManager/EmployeeList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManager
+{
+    class EmployeeList
+    {
+        List<Employee> employeeList;
+        public EmployeeList()
+        {
+            employeeList = new List<Employee>();
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            employeeList.Add(employee);
+        }
+
+        public void ListEmployees()
+        {
+            int i = 1;
+            foreach(var employee in employeeList)
+            {
+                Console.WriteLine(i + ". " + employee.ToString());
+                i++;
+            }
+        }
+
+        public void RemoveEmployee(int pos)
+        {
+            employeeList.RemoveAt(pos);
+        }
+    }
+}
--- EmployeeManager/Program.cs
+++ EmployeeManager/Program.cs
@@ -7,10 +7,33 @@ namespace EmployeeManager
     {
         static void Main(string[] args)
         {
-            var employee = new Employee();
-            Console.WriteLine("Employee Name: " + employee.name);
-            Console.WriteLine("Employee ID: " + employee.id);
-            Console.WriteLine("The weekly wage is " + string.Format("{0:0.00}", employee.wage));
+            EmployeeList empList = new EmployeeList();
+            int choice = 0;
+            while(choice != 4)
+            {
+                Console.WriteLine("1. Add Employee");
+                Console.WriteLine("2. List Employees");
+                Console.WriteLine("3. Remove Employee");
+                Console.WriteLine("4. Quit");
+                choice = Int32.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        var employee = new Employee();
+                        empList.AddEmployee(employee);
+                        break;
+                    case 2:
+                        empList.ListEmployees();
+                        break;
+                    case 3:
+                        Console.WriteLine("Please input position of employee to be deleted");
+                        int position = Int32.Parse(Console.ReadLine());
+                        empList.RemoveEmployee(position);
+                        break;
+                    case 4:
+                        return;
+                }
+            }
         }
     }
 }
