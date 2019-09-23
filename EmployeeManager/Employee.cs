cab7ead62cd2ac0bfa2d413ebfea57c5fc5591ab
Cristian <tudor.cristian.gabriel@gmail.com>
2019-09-23 19:29:41 +0100

    Incorporated Employee class and moved methods

---
+++ EmployeeManager/Employee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManager
+{
+    public class Employee
+    {
+        public string name, id;
+        public int hours;
+        public double hourlyRate;
+        public double wage;
+
+        internal Employee()
+        {
+            GetHours();
+            GetID();
+            GetName();
+            this.hourlyRate = 9.5;
+            CalcWage();
+        }
+        internal Employee(string name, string id, int hours)
+        {
+            this.name = name;
+            this.id = id;
+            this.hours = hours;
+            this.hourlyRate = 9.5;
+            CalcWage();
+        }
+
+        override
+        public string ToString()
+        {
+            return name + " " + "(" + id + ")";
+        }
+
+        private bool IsValidName(string name)
+        {
+            return (name.Length < 1 || name.Length > 50);
+        }
+        private string GetName()
+        {
+            Console.WriteLine("Please enter the employee name");
+            name = Console.ReadLine();
+            return !IsValidName(name) ? name : GetName();
+        }
+        private bool IsIdValid(string id)
+        {
+            char[] ids = id.ToCharArray();
+            return id.Length != 3 || !Char.IsLetter(ids[0]) || !Char.IsDigit(ids[1]) || !Char.IsDigit(ids[2]);
+        }
+        private string GetID()
+        {
+            Console.WriteLine("Please enter the employee ID");
+            id = Console.ReadLine();
+            return !IsIdValid(id) ? id : GetID();
+        }
+        private int GetHours()
+        {
+            Console.WriteLine("Please enter the hours worked");
+            hours = Int32.Parse(Console.ReadLine());
+            return (hours < 1 || hours > 100) ? GetHours() : hours;
+        }
+        public void CalcWage()
+        {
+            wage = hours <= 40 ? hours * hourlyRate : 40 * 9.5 + (hours - 40) * 14.25;
+        }
+    }
+
+}
--- EmployeeManager/Program.cs
+++ EmployeeManager/Program.cs
@@ -7,45 +7,10 @@ namespace EmployeeManager
     {
         static void Main(string[] args)
         {
-            string name = GetName();
-            string id = GetID();
-            int hours = GetHours();
-            double wage = CalcWage(hours);
-            Console.WriteLine("Employee Name: " + name);
-            Console.WriteLine("Employee ID: " + id);
-            Console.WriteLine("The weekly wage is " + string.Format("{0:0.00}", wage));
-        }
-
-        static private bool IsValidName(string name)
-        {
-            return (name.Length < 1 || name.Length > 50);
-        }
-        static private string GetName()
-        {
-            Console.WriteLine("Please enter the employee name");
-            string name = Console.ReadLine();
-            return !IsValidName(name) ? name : GetName();
-        }
-        static private bool IsIdValid(string id)
-        {
-            char[] ids = id.ToCharArray();
-            return id.Length != 3 || !Char.IsLetter(ids[0]) || !Char.IsDigit(ids[1]) || !Char.IsDigit(ids[2]);
-        }
-        static private string GetID()
-        {
-            Console.WriteLine("Please enter the employee ID");
-            string id = Console.ReadLine();
-            return !IsIdValid(id) ? id : GetID();
-        }
-        static private int GetHours()
-        {
-            Console.WriteLine("Please enter the hours worked");
-            int hours = Int32.Parse(Console.ReadLine());
-            return (hours < 1 || hours > 100) ? GetHours() : hours;
-        }
-        static private double CalcWage(int hours)
-        {
-            return hours <= 40 ? hours * 9.5 : 40 * 9.5 + (hours - 40) * 14.25;
+            var employee = new Employee();
+            Console.WriteLine("Employee Name: " + employee.name);
+            Console.WriteLine("Employee ID: " + employee.id);
+            Console.WriteLine("The weekly wage is " + string.Format("{0:0.00}", employee.wage));
         }
     }
 }
