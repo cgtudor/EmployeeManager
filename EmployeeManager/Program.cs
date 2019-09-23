4cbff6d5deb5765062950ba54c64b3edcfddde18
Cristian <tudor.cristian.gabriel@gmail.com>
2019-09-23 17:40:12 +0100

    Deleted useless comments

--- EmployeeManager/Program.cs
+++ EmployeeManager/Program.cs
@@ -10,8 +10,6 @@ namespace EmployeeManager
             int hours = Int32.Parse(Console.ReadLine());
             double wage = hours <= 40 ? hours * 9.5 : 40 * 9.5 + (hours-40) * 14.25;
             Console.WriteLine("The weekly wage is " + string.Format("{0:0.00}", wage));
-            //Employee done
-            Console.WriteLine("wow");
         }
     }
 }
