fc838d014bf737c8de4618630718d87e80cdfadb
Cristian <tudor.cristian.gabriel@gmail.com>
2019-09-23 18:01:57 +0100

    Added input and display for name and ID

--- EmployeeManager/Program.cs
+++ EmployeeManager/Program.cs
@@ -6,9 +6,15 @@ namespace EmployeeManager
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter the employee name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter the employee ID");
+            string id = Console.ReadLine();
             Console.WriteLine("Please enter the hours worked");
             int hours = Int32.Parse(Console.ReadLine());
             double wage = hours <= 40 ? hours * 9.5 : 40 * 9.5 + (hours-40) * 14.25;
+            Console.WriteLine("Employee Name: " + name);
+            Console.WriteLine("Employee ID: " + id);
             Console.WriteLine("The weekly wage is " + string.Format("{0:0.00}", wage));
         }
     }
